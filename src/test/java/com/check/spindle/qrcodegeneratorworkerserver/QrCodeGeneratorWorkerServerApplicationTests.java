52fc9f4f7547cc84dd4fdf61f838c076701a09de
howel <howelvchan@gmail.com>
2022-02-01 01:10:23 -0500

    Initial commit

---
+++ src/test/java/com/check/spindle/qrcodegeneratorworkerserver/QrCodeGeneratorWorkerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.check.spindle.qrcodegeneratorworkerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeGeneratorWorkerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
